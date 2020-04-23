3d37c957ca9861adfe768200ba385b73969147ac
Tony Nguyen <tonynguyen201@gmail.com>
2020-04-23 11:28:36 -0500

    Create queries and mutations for beers and breweries

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,30 @@
-const Mutations = {};
+const Mutations = {
+  async createBeer(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+
+    const beer = await ctx.db.mutation.createBeer(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+    return beer;
+  },
+
+  async createBrewery(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+    const brewery = await ctx.db.mutation.createBrewery(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+    return brewery;
+  }
+};
 
 module.exports = Mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,16 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+  beers: forwardTo('db'),
+  breweries: forwardTo('db')
+  // async beers(parent, args, ctx, info) {
+  //   const beers = await ctx.db.query.beers();
+  //   return beers;
+  // },
+  // async breweries(parent, args, ctx, info) {
+  //   const breweries = await ctx.db.query.breweries();
+  //   return breweries;
+  // }
+};
 
 module.exports = Query;
