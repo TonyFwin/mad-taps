3bd7bf737e35649b2a2a3f6fd26721d068620703
Tony Nguyen <tonynguyen201@gmail.com>
2020-04-27 10:14:16 -0500

    Pull in and display beers from graphql

---
+++ frontend/components/Beer.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+import Title from './styles/Title';
+import ItemStyles from './styles/ItemStyles';
+import PriceTag from './styles/PriceTag';
+import formatMoney from '../lib/formatMoney';
+import Item from './styles/ItemStyles';
+
+export default class Beer extends Component {
+  static propTypes = {
+    beer: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+      ibu: PropTypes.number.isRequired,
+      abv: PropTypes.number.isRequired,
+      description: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired,
+      largeImage: PropTypes.string.isRequired
+    })
+  };
+
+  render() {
+    const { beer } = this.props;
+    return (
+      <ItemStyles>
+        {beer.image && <img src={beer.image} alt={beer.name} />}
+        <Title>
+          <Link
+            href={{
+              pathname: '/beer',
+              query: { id: beer.id }
+            }}
+          >
+            <a>{beer.name}</a>
+          </Link>
+        </Title>
+        <PriceTag>{formatMoney(beer.price)}</PriceTag>
+        <p>{beer.description}</p>
+        <div className='buttonList'>
+          <Link
+            href={{
+              pathname: `update`,
+              query: { id: beer.id }
+            }}
+          >
+            <a>Edit 📝</a>
+          </Link>
+          <button> Add to Card</button>
+          <button>Delete</button>
+        </div>
+      </ItemStyles>
+    );
+  }
+}
--- frontend/components/Beers.js
+++ frontend/components/Beers.js
@@ -1,13 +1,54 @@
 import React, { Component } from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
+import styled from 'styled-components';
+import Beer from './Beer';
+
+const ALL_BEERS_QUERY = gql`
+  query ALL_BEERS_QUERY {
+    beers {
+      id
+      name
+      price
+      ibu
+      abv
+      description
+      image
+      largeImage
+    }
+  }
+`;
+
+const Center = styled.div`
+  text-align: center;
+`;
+
+const BeerList = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 50px;
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+`;
 
 export default class Beers extends Component {
   render() {
     return (
-      <div>
-        <p>Beers!</p>
-      </div>
+      <Center>
+        <Query query={ALL_BEERS_QUERY}>
+          {({ data, error, loading }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Error: {error.message}</p>;
+            return (
+              <BeerList>
+                {data.beers.map(beer => (
+                  <Beer key={beer.id} beer={beer} />
+                ))}
+              </BeerList>
+            );
+          }}
+        </Query>
+      </Center>
     );
   }
 }
