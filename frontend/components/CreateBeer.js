3a47762844c4be8bfe226a24db09cc90c5b43298
Tony Nguyen <tonynguyen201@gmail.com>
2020-04-29 12:04:56 -0500

    Added image uploading functionality to createBeer

--- frontend/components/CreateBeer.js
+++ frontend/components/CreateBeer.js
@@ -51,6 +51,28 @@ export default class CreateBeer extends Component {
     this.setState({ [name]: val });
   };
 
+  uploadFile = async e => {
+    console.log('uploading file');
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'madtaps');
+
+    const res = await fetch(
+      'https://api.cloudinary.com/v1_1/dvgb0d5po/image/upload',
+      {
+        method: 'POST',
+        body: data
+      }
+    );
+    const file = await res.json();
+    console.log(file);
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    });
+  };
+
   render() {
     return (
       <Mutation mutation={CREATE_BEER_MUTATION} variables={this.state}>
@@ -69,6 +91,24 @@ export default class CreateBeer extends Component {
           >
             <ErrorMessage error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor='file'>
+                Image
+                <input
+                  type='file'
+                  id='file'
+                  name='file'
+                  placeholder='Upload an image'
+                  required
+                  onChange={this.uploadFile}
+                />
+                {this.state.image && (
+                  <img
+                    width='200'
+                    src={this.state.image}
+                    alt='Upload Preview'
+                  />
+                )}
+              </label>
               <label htmlFor='name'>
                 Beer Name
                 <input
