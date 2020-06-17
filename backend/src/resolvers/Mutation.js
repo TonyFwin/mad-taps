991f984146e97c11277dff863b2ca919fb4904e4
Tony Nguyen <tonynguyen201@gmail.com>
2020-06-16 20:38:50 -0500

    Add signin functionality to backend

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -106,6 +106,28 @@ const Mutations = {
     });
     // finally return user to the browser
     return user;
+  },
+
+  async signin(parent, { email, password }, ctx, info) {
+    // check if there is a user with email
+    const user = await ctx.db.query.user({ where: { email } });
+    if (!user) {
+      throw new Error(`No such user found email ${email}`);
+    }
+    // check if their password is correct
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new Error('Invalid Password!');
+    }
+    // generate JWT token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // set cookie with the token
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
+    });
+    // return the user
+    return user;
   }
 };
 
