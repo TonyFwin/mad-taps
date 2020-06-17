f8a9ffd7183fcb8c5f224a3dd8627c7638e61783
Tony Nguyen <tonynguyen201@gmail.com>
2020-06-16 20:39:05 -0500

    Create signin component

---
+++ frontend/components/SignIn.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import Form from './styles/Form';
+import ErrorMessage from './ErrorMessage';
+import { CURRENT_USER_QUERY } from './User';
+
+const SIGNIN_MUTATION = gql`
+  mutation SIGNIN_MUTATION($email: String!, $password: String!) {
+    signin(email: $email, password: $password) {
+      id
+      email
+      name
+    }
+  }
+`;
+
+class Signin extends Component {
+  state = {
+    name: '',
+    password: '',
+    email: ''
+  };
+
+  saveToState = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    return (
+      <Mutation
+        mutation={SIGNIN_MUTATION}
+        variables={this.state}
+        refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+      >
+        {(signin, { error, loading }) => {
+          return (
+            <Form
+              method='post'
+              onSubmit={async e => {
+                e.preventDefault();
+                const res = await signin();
+                console.log(res);
+                this.setState({ name: '', email: '', password: '' });
+              }}
+            >
+              <fieldset disabled={loading} aria-busy={loading}>
+                <h2>Sign Into Your Account </h2>
+                <ErrorMessage error={error} />
+                <label htmlFor='email'>
+                  Email
+                  <input
+                    type='email'
+                    name='email'
+                    placeholder='email'
+                    value={this.state.email}
+                    onChange={this.saveToState}
+                  />
+                </label>
+                <label htmlFor='password'>
+                  Password
+                  <input
+                    type='password'
+                    name='password'
+                    placeholder='password'
+                    value={this.state.password}
+                    onChange={this.saveToState}
+                  />
+                </label>
+                <button type='submit'>Sign In!</button>
+              </fieldset>
+            </Form>
+          );
+        }}
+      </Mutation>
+    );
+  }
+}
+export default Signin;
