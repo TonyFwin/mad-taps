cc69eeeb5249795ca91bf66dea94e74a5809dc29
Tony Nguyen <tonynguyen201@gmail.com>
2020-06-16 20:55:52 -0500

    Update nav.js to show only certain links to users when signed in

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -7,35 +7,36 @@ import User from './User';
 
 const Nav = props => {
   return (
-    <NavStyles>
-      <User>
-        {({ data: { me } }) => {
-          console.log(me);
-          if (me) {
-            return <p>{me.name}</p>;
-          }
-          return null;
-        }}
-      </User>
-      <Link href='/beers'>
-        <a>Shop</a>
-      </Link>
-      <Link href='/sell'>
-        <a>Sell</a>
-      </Link>
-      <Link href='/breweries'>
-        <a>Breweries</a>
-      </Link>
-      <Link href='/signup'>
-        <a>Signup</a>
-      </Link>
-      <Link href='/orders'>
-        <a>Orders</a>
-      </Link>
-      <Link href='/me'>
-        <a>Account</a>
-      </Link>
-    </NavStyles>
+    <User>
+      {({ data: { me } }) => (
+        <NavStyles>
+          <Link href='/beers'>
+            <a>Shop</a>
+          </Link>
+          <Link href='/breweries'>
+            <a>Breweries</a>
+          </Link>
+          {me && (
+            <>
+              <Link href='/sell'>
+                <a>Sell</a>
+              </Link>
+              <Link href='/orders'>
+                <a>Orders</a>
+              </Link>
+              <Link href='/me'>
+                <a>Account</a>
+              </Link>
+            </>
+          )}
+          {!me && (
+            <Link href='/signup'>
+              <a>Sign in</a>
+            </Link>
+          )}
+        </NavStyles>
+      )}
+    </User>
   );
 };
 
